8be269f9dd1c0d72e970407951fbb30e2ea3a760
Khushi A <99123453+KhushiiAgarwal@users.noreply.github.com>
2024-02-29 00:14:33 +0530

    Multiple Linear Regression

---
+++ MLR.r
@@ -0,0 +1,12 @@
+setwd("D:/Practical/VIT/DS R")
+t1=read.csv("Toy_sales_csv.csv")
+
+#MLR----
+print("--MLR--")
+l2<-lm(Unitsales~Price+Adexp+Promexp,t1)
+s2=summary(l2)
+print(s2)
+df=data.frame(Price=c(9.1,8.1),Adexp=c(52,50),Promexp=c(61,60))
+pred2=predict(l2,df)
+df1<-cbind(df,pred2)
+print(df1)
